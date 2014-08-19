762f778e5c18d8446070afb63aa4ce14f3be8131
adammartin <ebondark.od@gmail.com>
2014-08-19 09:51:23 -0500

    Maximizing windows for mac

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(auto-indent-delete-trailing-whitespace-on-save-file t)
  '(auto-indent-on-visit-file t)
  '(enh-ruby-indent-tabs-mode t)
